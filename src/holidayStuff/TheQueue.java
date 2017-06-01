3c7d57f5e7eb7641e160b49700194165946688f6
hp\Alexandre <baizoukoualexandre@yahoo.fr>
2017-06-01 14:28:59 +0100

    priority queue not compiling

--- src/holidayStuff/TheQueue.java
+++ src/holidayStuff/TheQueue.java
@@ -3,9 +3,10 @@ package holidayStuff;
 import java.util.Arrays;
 
 public class TheQueue {
+	private static final String input = null;
 	private String[] queueArray;
 	private int queueSize;
-	private int front, rear, numberOfItems = 0;
+	private int front, end, numberOfItems = 0;
 	TheQueue(int size){
 		queueSize = size;
 		queueArray = new String[size];
@@ -14,8 +15,8 @@ public class TheQueue {
 	}
 	public void insert(String input){
 		if(numberOfItems + 1 <= queueSize){
-			queueArray[rear] = input;
-			rear++; // increment rear to increase the size of array
+			queueArray[end] = input;
+			end++; // increment rear to increase the size of array
 			numberOfItems++;
 			System.out.println("INSERT" + input + "Was Added to the Queue\n");
 		}else{
@@ -38,13 +39,42 @@ public class TheQueue {
 	public void peek(){
 		System.out.println("The first element is " + queueArray[front] );
 	}
+	
+	public void priorityInsert(String string){
+		int i;
+		 
+		if(numberOfItems == 0){
+			insert(input);
+		}else{
+			for(i=numberOfItems - 1; i>= 0; i--){
+				if(Integer.parseInt(input)> Integer.parseInt(queueArray[i])){
+					queueArray[i+1] = queueArray[i];
+					
+				}else break;
+				
+						}
+			queueArray[i+1] = input;
+			end++;
+			numberOfItems++;
+		}
+	}
 	public static void main(String[] args){
 		
 		TheQueue theQueue = new TheQueue(20);
-		theQueue.insert("20");
-		theQueue.insert("25");
-		theQueue.insert("29");
+//		theQueue.insert("20");
+//		theQueue.insert("25");
+//		theQueue.insert("29");
+		
+		theQueue.priorityInsert("20");
+		theQueue.priorityInsert("25");
+		theQueue.priorityInsert("15");
+		theQueue.priorityInsert("9");
+		
+		theQueue.remove();
+		theQueue.remove();
+		
 		theQueue.displayTheQueue();
+		theQueue.peek();
 		
 	}
 	private void displayTheQueue() {
